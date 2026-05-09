abc2f016d97df780b19d4f3b0aa0a4927424052b
nandu <nandakishore695@gmail.com>
2026-05-09 08:40:58 +0530

    comment mongoose

--- src/_lib/model/Blogs.js
+++ src/_lib/model/Blogs.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const blogsSchema = new mongoose.Schema({ 
-    blogImage:{type: String , required: true},
-    blogTitle: {type: String, required: true},
-    blogDescription:{type: String , required: true},
-    blogCategory:{type: String , required: true},
-    createDate:{type: Date, default: Date.now}
-});
-export const NewBlog = mongoose.models.NewBlog || mongoose.model("NewBlog", blogsSchema);
+// const blogsSchema = new mongoose.Schema({ 
+//     blogImage:{type: String , required: true},
+//     blogTitle: {type: String, required: true},
+//     blogDescription:{type: String , required: true},
+//     blogCategory:{type: String , required: true},
+//     createDate:{type: Date, default: Date.now}
+// });
+// export const NewBlog = mongoose.models.NewBlog || mongoose.model("NewBlog", blogsSchema);
--- src/_lib/model/Email_Model.js
+++ src/_lib/model/Email_Model.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const emailSchema = mongoose.Schema({
-  isEmailId: { type: String, required: true, unique: true },
-  createDate: { type: Date, default: Date.now },
-});
+// const emailSchema = mongoose.Schema({
+//   isEmailId: { type: String, required: true, unique: true },
+//   createDate: { type: Date, default: Date.now },
+// });
 
-export const UserEmail = mongoose.models.UserEmail || mongoose.model("UserEmail", emailSchema);
+// export const UserEmail = mongoose.models.UserEmail || mongoose.model("UserEmail", emailSchema);
